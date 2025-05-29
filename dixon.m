02d27caa1548c79798761be92394221b57a08b79
Haohai <163224857+qimitu7@users.noreply.github.com>
2025-05-29 15:27:52 +0800

    Update dixon.m

--- dixon.m
+++ dixon.m
@@ -1,4 +1,4 @@
-function Lagrange1D(nodes, values, X)
+function Lagrange(nodes, values, X)
     k := #nodes;
     L := Parent(values[1])!0;
     for j in [1..k] do
@@ -18,11 +18,18 @@ end function;
 function TensorInterpolation(Vars, Grids, Vals, P)
     d := #Vars;
     if d eq 1 then
-        return Lagrange1D(Grids[1], Vals, Vars[1]); 
+        return Lagrange(Grids[1], Vals, Vars[1]); 
     end if;
     blockSize := &* [ #Grids[i] : i in [1..d-1] ];
     Hd := [];
+    
+    s := [];
     for j in [1..#Grids[d]] do
+        for _ in [1..#s] do
+            printf "\b";
+        end for;
+        s := Sprint(j) cat "/" cat Sprint(#Grids[d]) cat " ";
+        printf s;
         subVals := Vals[(j-1)*blockSize + 1 .. j*blockSize];
         H_j := TensorInterpolation(
                    Vars[1..d-1],
@@ -32,16 +39,12 @@ function TensorInterpolation(Vars, Grids, Vals, P)
                );
         Append(~Hd, H_j);
     end for;
-
     Xd := Vars[d];
-
     deg_d := Max([ Degree(h, Xd) : h in Hd ]);
-
     C := [];
     for k in [0..deg_d] do
         seq := [ Coefficient(Hd[j], Xd, k) : j in [1..#Grids[d]] ];
-print Grids;
-        Ck := Lagrange1D(Grids[d], seq, Xd);
+        Ck := Lagrange(Grids[d], seq, Xd);
         Append(~C, Ck);
     end for;
 
@@ -108,16 +111,92 @@ function BuildCoefficientMatrix(DM, d0, d1, nv, np, gens)
     return dix, dixR;
 end function;
 
-function dixon(ps, vars, pars, is_interpolation)
+function SGCD(M)
+    R := BaseRing(M);
+    nrows := NumberOfRows(M);
+    ncols := NumberOfColumns(M);
 
-print(1);
+    gcds := [];
 
+    for i in [1..nrows] do
+        row := [M[i][j] : j in [1..ncols]];
+        g := row[1];
+        for j in [2..#row] do
+            g := GCD(g, row[j]);
+            if IsOne(g) then
+                break;
+    end if;
+        end for;
+
+        if not IsOne(g) then
+            Append(~gcds, g);
+            for j in [1..ncols] do
+                ;//M[i][j] := M[i][j] div g;
+            end for;
+        end if;
+    end for;
+
+    for j in [1..ncols] do
+        col := [M[i][j] : i in [1..nrows]];
+        g := col[1];
+        for i in [2..#col] do
+            g := GCD(g, col[i]);
+            if IsOne(g) then
+                break;
+    end if;
+        end for;
+        
+        if not IsOne(g) then
+            Append(~gcds, g);
+            for i in [1..nrows] do
+                ;//M[i][j] := M[i][j] div g;
+            end for;
+        end if;
+    end for;
+
+    return M, gcds;
+end function;
+
+function ExtractParameters(ps, vars)
+    R := Parent(ps[1]);
+    n := Rank(R);
+    basis := [R.i : i in [1..n]];
+    used := [false : i in [1..n]];
+
+    for f in ps do
+        mons := Monomials(f); 
+        for m in mons do
+            exps := Exponents(m); 
+            for i in [1..n] do
+                if exps[i] ne 0 then
+                    used[i] := true;
+                end if;
+            end for;
+        end for;
+    end for;
+
+    vars_set := Seqset(vars);
+    pars := [basis[i] : i in [1..n] | used[i] and not basis[i] in vars_set];
+    return pars;
+end function;
+
+function dixon(ps, vars, is_interpolation)
+
+print("\nStep0: Preprocessing\n");
+
+    if #vars ne #ps - 1 then
+        error "Number of variables must be equal to the number of equations minus one.";
+    end if;
+
+    pars := ExtractParameters(ps, vars);
+//print pars;
+    print "Moving original variables and polynomials into a new polynomial ring...";
     R0 := Universe(ps);
     K := CoefficientRing(R0);
     ds := &*[Degree(p) : p in ps];
-    print ds, #K;
-    if ds gt #K-1 then
-        ds := #K-1;
+    //print ds, #K;
+    if ds gt #K-2 then
+        ds := #K-2;
     end if;
 
     allGens := [ R0.i : i in [1..Ngens(R0)] ];
@@ -127,13 +206,21 @@ print(1);
 
     nv := #varIdx;
     np := #parIdx;
+    R0names := Names(R0);
     if np eq 1 then
         P<x> := PolynomialRing(K);
     else
         P := PolynomialRing(K, np);
+        paraNames := [ R0names[i] : i in parIdx ];
+        AssignNames(~P, paraNames);
     end if;
     FR := FunctionField(P);
     R := PolynomialRing(FR, 2*nv);
+
+    origNames := [ R0names[i] : i in varIdx ];
+    dualNames := [ "~" cat s : s in origNames ];
+    newNames := origNames cat dualNames;
+    AssignNames(~R, newNames);
 
     gensR := [ R.i : i in [1..2*nv] ];
     gensP := [ P.i : i in [1..np] ];
@@ -156,6 +243,8 @@ print(1);
 
     psd := [ phi(f) : f in ps ];
 
+print("\nStep1: Construct the Cancellation Matrix and Dixon Polynomial\n");
+
     M := ZeroMatrix(R, nv+1, nv+1);
     for i in [1..nv+1] do
         subst := [ k le i-1 select gensR[nv+k] else gensR[k] : k in [1..2*nv] ];
@@ -163,28 +252,49 @@ print(1);
             M[i,j] := Evaluate(psd[j], subst);
         end for;
     end for;
+print "Size of Cancellation Matrix:", nv+1, nv+1;
+//print("Cancellation Matrix:");
+//print M;
+print("Computing Dixon Polynomial...");
+    if false then
+        time DM := Determinant(M);
+        factors := [ gensR[i] - gensR[nv+i] : i in [1..nv] ];
+        for f in factors do
+            Q := DM div f;
+            DM := Q;
+        end for;
+    else
+        rows := [ M[i] : i in [1..nv+1] ];  // nv+1 rows
+        newrows := [rows[1]];  // First row unchanged
+        for i in [1..nv] do
+            delta := rows[i+1] - rows[i];
+            denom := gensR[i] - gensR[nv+i];
+            newrow := delta div denom;  // Symbolic division (exact)
+            Append(~newrows, newrow);
+        end for;
+        newM := Matrix(newrows);
+        time DM := Determinant(newM);
+    end if;
+//print M,newM;
 
-print(2);
+//print("Dixon Polynomial:");
+//print DM;
 
-    time DM := Determinant(M);
-    factors := [ gensR[i] - gensR[nv+i] : i in [1..nv] ];
-    
-    time for f in factors do
-        Q := DM div f;
-        DM := Q;
-    end for;
-
-print(3);
+print("\nStep2: Build the Dixon Matrix\n");
 
     d0 := [Degree(DM, gensR[i]) + 1 : i in [1..nv]];
     d1 := [Degree(DM, gensR[nv + i]) + 1 : i in [1..nv]];
-    
+print("Degree of each variable: ");
+print d0,d1; 
     time dix, dixM := BuildCoefficientMatrix(DM, d0, d1, nv, np, gensR);
-    print Parent(dixM[1,1]);
-    print NumberOfRows(dixM), NumberOfColumns(dixM);
-print(4);
+print "Size of Dixon Matrix: ", NumberOfRows(dixM), NumberOfColumns(dixM);
+//print("Dixon Matrix: ");
+//print dix;
 
-    time E := EchelonForm(dixM);
+
+print("\nStep3: Extract Maximal Rank Submatrix\n");
+
+    E := EchelonForm(dixM);
     rowPivots := [];
     time for i in [1..Nrows(E)] do
         row := Eltseq(E[i]);
@@ -197,7 +307,7 @@ print(4);
     end for;
     rowPivots := [pos : pos in rowPivots | pos ne 0];
     
-    time ET := EchelonForm(Transpose(dixM));
+    ET := EchelonForm(Transpose(dixM));
     colPivots := [];
     for i in [1..Nrows(ET)] do
         row := Eltseq(ET[i]);
@@ -209,92 +319,142 @@ print(4);
         end for;
     end for;
     colPivots := [pos : pos in colPivots | pos ne 0];
-    
+
     dix2 := Submatrix(dix, colPivots, rowPivots);
 
-    print NumberOfRows(dix2), NumberOfColumns(dix2);
-print(6);
+    dix2, gcds := SGCD(dix2);
 
-    if is_interpolation eq 0 then
-        time d1 := Determinant(dix2);
-        if np eq 1 then
-            return d1/LeadingCoefficient(d1);
-            inv_images := R0.parIdx[1];
-        else
-            inv_images := [R0.parIdx[k]: k in [1..np]];
-        end if;
-        d2 := Evaluate(d1, inv_images);
-        d2 := d2/LeadingCoefficient(d2);
+print "Size of Maximal Rank Submatrix: ", NumberOfRows(dix2), NumberOfColumns(dix2);
+//print("Maximal Rank Submatrix: ");
+//print(dix2);
 
-        return d2;
-    else
+print("\nStep4: Compute the Dixon Resultant\n");
+if is_interpolation eq 1 then
+    print("Using Interpolation");
+else
+    print("Using Gauss elimination");
+end if;
+    if IsFinite(K) then
         pe := PrimitiveElement(K);
-        if np eq 1 then 
-            points := [ pe^i : i in [1..ds] ] cat [K!0];
+    else
+        pe := 2;
+    end if;
+    extf := [];
+    if np eq 1 then
+        if is_interpolation eq 0 then
+            time d1 := Determinant(dix2);
+        else
+            points := [ pe^i : i in [0..ds] ] cat [K!0];
             values := [ ];
-            ii := 0;
+            npoint := ds+2;
+            ipoint := 0;
+            s := [];
+print "Computing Interpolation Points...";
             time for a in points do
-                ii := ii + 1;
-                print ii, a;
+                ipoint := ipoint + 1;
+                for j in [1..#s] do
+                    printf "\b";
+                end for;
+                s := Sprint(ipoint) cat "/" cat Sprint(npoint) cat " ";
+                printf s;
                 N := Evaluate(dix2, a); 
                 Append(~values, Determinant(N)); 
             end for;
-            time d2 := Interpolation(points, values);
-            d2 := d2/LeadingCoefficient(d2);
-            return d2;
+print "Interpolating...";
+            d1 := Interpolation(points, values);
+        end if;
+        d2 := d1/LeadingCoefficient(d1);
+        // inv_images := R0.parIdx[1];
+        // d2 := Evaluate(d2, inv_images);
+//print "Dixon Resultant:", d2;
+    else
+        if is_interpolation eq 0 then
+            time d1 := Determinant(dix2);
         else
             uppers := [ds : _ in [1..np]];
             grids := [];
+
             for u in uppers do
-                Append(~grids, [ pe^i : i in [1..u]] cat [K!0] );
+                Append(~grids, [ pe^i : i in [0..u]] cat [K!0] );
             end for;
+//print grids;
             vals := [];
-            npoint := (ds+1)^np;
+            npoint := (ds+2)^np;
             ipoint := 0;
-            for point in CartesianProduct([ grids[k] : k in [1..np] ]) do
+            s := [];
+print "Computing Interpolation Network...";
+            time for point in CartesianProduct([ grids[k] : k in [1..np] ]) do
                 reversed_point := [ point[np - i + 1] : i in [1..np] ];
                  M_eval := Matrix(K, Nrows(dix2), Ncols(dix2), 
                           [Evaluate(elt, reversed_point) : elt in Eltseq(dix2)]);
                 Append(~vals, Determinant(M_eval)); 
                 ipoint := ipoint + 1;
-                print ipoint,npoint;
+                for j in [1..#s] do
+                    printf "\b";
+                end for;
+                s := Sprint(ipoint) cat "/" cat Sprint(npoint) cat " ";
+                printf s;
             end for;
-
             vars :=  [ P.i : i in [1..Ngens(P)] ];
+print "Interpolating...";
             time d1 := TensorInterpolation(vars, grids, vals, P);
-            // time d1 := Determinant(dix2);
-            if np eq 1 then
-                return d1;
-                inv_images := R0.parIdx[1];
-            else
-                inv_images := [R0.parIdx[k]: k in [1..np]];
-            end if;
-            d2 := Evaluate(d1, inv_images);
-            d2 := d2/LeadingCoefficient(d2);
-            return d2;
         end if;
+        d2 := d1/LeadingCoefficient(d1);
+//print("Dixon Resultant:");
+//print d2;
+print("\nStep5: Remove Extraneous Factors\n");
+        inv_images := [R0.parIdx[k]: k in [1..np]];
+        d2 := Evaluate(d2, inv_images);
+        time fac := Factorization(d2);
+        ps_eval := [];
+        evals := [];
+        for i in [1..Ngens(R0)] do
+            if i in varIdx then
+                Append(~evals, allGens[i]);
+            elif i in parIdx then
+                if i eq parIdx[1] then
+                    Append(~evals, allGens[i]);
+                else
+                    Append(~evals, R0!pe^i);
+                end if;
+            else
+                Append(~evals, R0!0);
+            end if;
+        end for;
+        for i in [1..#ps] do
+            Append(~ps_eval, Evaluate(ps[i], evals));
+        end for;
+        I := ideal<R0|ps_eval>;
+        G := GroebnerBasis(I);
+        g := G[#G];
+        d2f := [];
+
+        for i in [1..#fac] do
+            fac_eval := Evaluate(fac[i][1], evals);
+            if not (Resultant(fac_eval, g, pars[1]) eq 0) then
+                Append(~extf, fac[i][1]);
+            else
+                Append(~d2f, fac[i][1]);
+            end if;
+        end for;
+        if #d2f eq 0 then
+            d2 := &*extf;
+        else
+            d2 := &*d2f;
+        end if;
+print("Extraneous Factors:");
+print extf;
     end if;
-print(7);
+    return d2, gcds, extf;
 end function;
 
+//SetColumns(170);
+
 K := GF(65537);
-R< x0, x1, x2, x3 >  := PolynomialRing(K, 4);
-ps := [4748*x0^2 + 11101*x0*x1 - 6109*x1^2 - 31021*x0*x2 + 9318*x1*x2 + 29439*x2^2 - 21354*x0*x3 + 24482*x1*x3 + 21080*x2*x3 - 16286*x3^2 - 28333*x0 + 18483*x1 - 31088*x2 - 12606*x3 - 14615,
- 1124*x0^2 + 24743*x0*x1 + 4039*x1^2 - 7804*x0*x2 + 26702*x1*x2 + 15844*x2^2 + 3073*x0*x3 + 275*x1*x3 - 15498*x2*x3 - 4836*x3^2 + 22767*x0 + 4408*x1 - 26504*x2 + 32131*x3 + 16732,
- 6706*x0^2 + 1956*x0*x1 - 13851*x1^2 - 7861*x0*x2 - 8872*x1*x2 + 27248*x2^2 + 28478*x0*x3 - 13449*x1*x3 - 12087*x2*x3 + 31514*x3^2 + 29552*x0 + 25623*x1 - 12628*x2 - 14744*x3 - 30708];
+R< x0, x1, x2, x3>  := PolynomialRing(K, 4);
+ps := [3*x0^2*x2^2*x3 + 5*x0*x1*x2*x3^2 - 2*x2^4 - 5*x0^2*x2 + 2*x1^2*x2 + 4*x0*x1 - x0 - 5,
+ 5*x0^3*x1*x2 - 4*x1^4*x2 + 3*x0^2*x1*x2^2 + 5*x1*x2^2*x3^2 - x2^3*x3^2 - 4*x0*x1*x2^2 + 2*x0*x1*x2 - 3*x0*x3 + 5*x2*x3,
+ -x0^5 + 4*x0^2*x1^3 + 5*x0^3*x1*x3 - 5*x0^3*x2*x3 + x0*x2^3*x3 + 3*x0*x1*x3^3 + 3*x1*x3^2 + 4*x0^2 + 4*x2];
 vars := [x0,x1];
-pars := [x3,x2];
 is_interpolation := 1;
-time d := dixon(ps, vars, pars, is_interpolation);
-
-//rs := Roots(d);
-
-I := ideal<R|ps>;
-//SetVerbose("Groebner", 1);
-IsZeroDimensional(I);
-Dimension(I);
-time G := GroebnerBasis(I);
-g := G[#G];
-// v := Variety(I);
-time r := Resultant(ps[2],ps[1],x0);
-r := r/LeadingCoefficient(r);
+time d := dixon(ps, vars, is_interpolation);
